4f77c3d93c9028474d8952d5b32c0f87882b0596
SHawnHardy <shawn_work@outlook.com>
2019-01-31 16:12:00 +0800

    [Update]ws_network  - Add namespace sh  - Add 	WsNetwork(int size, int regular_k, double reconnection_pr) :           Num_Vertices_(size), Num_Edges_(Num_Vertices_ * regular_k * 2),           Regular_K_(regular_k), Reconnection_Pr_(reconnection_pr);  - Add serialize for WsNetwork  - Add Unitest  - Improve code quality

--- include/ws_network.h
+++ include/ws_network.h
@@ -1,8 +1,8 @@
 /**
  * @file ws_network.h
- * @version v0.1
+ * @version v0.2
  * @author SHawnHardy
- * @date 2019-01-27
+ * @date 2019-01-31
  * @copyright MIT License
  */
 
@@ -10,110 +10,164 @@
 #define E_PTD_PS_WS_NETWORK_H
 
 #include <cmath>
+#include <cstring>
 #include <algorithm>
+#include <iostream>
 #include <random>
 
+namespace sh {
+    class WsNetwork {
+    public:
+        const int Num_Vertices_;
+        const int Num_Edges_;
+        const int Regular_K_;
+        const double Reconnection_Pr_;
 
-class WsNetwork {
-public:
-    const int Num_Vertices_;
-    const int Num_Edges_;
-    const int Regular_K_;
-    const double Reconnection_Pr_;
+        WsNetwork(int size, int regular_k, double reconnection_pr) :
+                Num_Vertices_(size), Num_Edges_(Num_Vertices_ * regular_k * 2),
+                Regular_K_(regular_k), Reconnection_Pr_(reconnection_pr) {
+            adjacency_matrix_ = new bool *[Num_Vertices_];
+            edges_ = new int[Num_Edges_];
+            forward_star_ = new int *[Num_Vertices_ + 1];
 
-    WsNetwork(int size, int regular_k, double reconnection_pr) :
-            Num_Vertices_(size), Regular_K_(regular_k), Reconnection_Pr_(reconnection_pr),
-            Num_Edges_(Num_Vertices_ * regular_k * 2) {
-        adjacency_matrix_ = new bool *[Num_Vertices_];
-        edges_ = new int[Num_Edges_ + 1];
-        start_ = new int *[Num_Vertices_];
-        end_ = new int *[Num_Vertices_];
-
-        for (int i = 0; i < Num_Vertices_; ++i) {
-            adjacency_matrix_[i] = new bool[Num_Vertices_]();
-            for (int j = 0; j < Num_Vertices_; ++j) {
-                if (i == j) {
-                    continue;
-                }
-                int dis = abs(i - j);
-                adjacency_matrix_[i][j] = (std::min(dis, Num_Vertices_ - dis) <= Regular_K_);
-            }
-        }
-
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> int_dtb(0, Num_Vertices_ - 1);
-        std::uniform_real_distribution<> real_dtb(0.0, 1.0);
-
-        for (int i = 1; i <= Regular_K_; ++i) {
-            for (int neu_s = 0; neu_s < Num_Vertices_; ++neu_s) {
-                if (real_dtb(gen) < Reconnection_Pr_) {
-                    int neu_t = (neu_s + i) % Num_Vertices_;
-                    adjacency_matrix_[neu_s][neu_t] = adjacency_matrix_[neu_t][neu_s] = false;
-                    int new_neu_t = int_dtb(gen);
-                    while (new_neu_t == neu_s || adjacency_matrix_[neu_s][new_neu_t]) {
-                        new_neu_t = int_dtb(gen);
+            for (int i = 0; i < Num_Vertices_; ++i) {
+                adjacency_matrix_[i] = new bool[Num_Vertices_]();
+                for (int j = 0; j < Num_Vertices_; ++j) {
+                    if (i == j) {
+                        continue;
                     }
-                    adjacency_matrix_[neu_s][new_neu_t] = adjacency_matrix_[new_neu_t][neu_s] = true;
+                    int dis = abs(i - j);
+                    adjacency_matrix_[i][j] = (std::min(dis, Num_Vertices_ - dis) <= Regular_K_);
                 }
             }
+
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> int_dtb(0, Num_Vertices_ - 1);
+            std::uniform_real_distribution<> real_dtb(0.0, 1.0);
+
+            for (int i = 1; i <= Regular_K_; ++i) {
+                for (int neu_s = 0; neu_s < Num_Vertices_; ++neu_s) {
+                    if (real_dtb(gen) < Reconnection_Pr_) {
+                        int neu_t = (neu_s + i) % Num_Vertices_;
+                        adjacency_matrix_[neu_s][neu_t] = adjacency_matrix_[neu_t][neu_s] = false;
+                        int new_neu_t = int_dtb(gen);
+                        while (new_neu_t == neu_s || adjacency_matrix_[neu_s][new_neu_t]) {
+                            new_neu_t = int_dtb(gen);
+                        }
+                        adjacency_matrix_[neu_s][new_neu_t] = adjacency_matrix_[new_neu_t][neu_s] = true;
+                    }
+                }
+            }
+
+            int *point = edges_;
+            for (int i = 0; i < Num_Vertices_; ++i) {
+                forward_star_[i] = point;
+                for (int j = 0; j < Num_Vertices_; ++j) {
+                    if (adjacency_matrix_[i][j]) {
+                        *point = j;
+                        ++point;
+                    }
+                }
+            }
+            forward_star_[Num_Vertices_] = point;
         }
 
-        int *point = edges_;
-        for (int i = 0; i < Num_Vertices_; ++i) {
-            start_[i] = point;
-            for (int j = 0; j < Num_Vertices_; ++j) {
-                if (adjacency_matrix_[i][j]) {
-                    *point = j;
+        WsNetwork(int num_vertices, int num_edges, int regular_k, double reconnection_pr,
+                  int edges[], int degree[]) :
+                Num_Vertices_(num_vertices), Num_Edges_(num_edges),
+                Regular_K_(regular_k), Reconnection_Pr_(reconnection_pr) {
+            adjacency_matrix_ = new bool *[Num_Vertices_];
+            edges_ = new int[Num_Edges_];
+            forward_star_ = new int *[Num_Vertices_ + 1];
+
+            memcpy(edges_, edges, sizeof(int) * num_edges);
+            int *point = edges_;
+            for (int i = 0; i < Num_Vertices_; i++) {
+                forward_star_[i] = point;
+
+                adjacency_matrix_[i] = new bool[Num_Vertices_]();
+                for (int j = 0; j < degree[i]; j++) {
+                    adjacency_matrix_[i][*point] = true;
                     ++point;
                 }
             }
-            end_[i] = point;
+            forward_star_[Num_Vertices_] = point;
         }
-    }
 
-    ~WsNetwork() {
-        for (int i = 0; i < Num_Vertices_; ++i) {
-            delete[] adjacency_matrix_[i];
+        ~WsNetwork() {
+            for (int i = 0; i < Num_Vertices_; ++i) {
+                delete[] adjacency_matrix_[i];
+            }
+            delete[] adjacency_matrix_;
         }
-        delete[] adjacency_matrix_;
-    }
 
-    std::pair<int *, int *> get_edges(int x) const {
-        return std::make_pair(start_[x], end_[x]);
-    }
+        std::pair<int *, int *> get_edges(int x) const {
+            return std::make_pair(forward_star_[x], forward_star_[x + 1]);
+        }
 
-    bool check(int x, int y) const {
-        return adjacency_matrix_[x][y];
-    }
+        bool check(int x, int y) const {
+            return adjacency_matrix_[x][y];
+        }
 
-    double get_clustering_coefficient(int x) const {
-        int lambda = 0;
-        auto edges = get_edges(x);
-        for (auto i = edges.first; i != edges.second; ++i) {
-            for (auto j = (i + 1); j != edges.second; ++j) {
-                if (check(*i, *j)) {
-                    ++lambda;
+        double get_clustering_coefficient(int x) const {
+            int lambda = 0;
+            auto edges = get_edges(x);
+            for (auto i = edges.first; i != edges.second; ++i) {
+                for (auto j = (i + 1); j != edges.second; ++j) {
+                    if (check(*i, *j)) {
+                        ++lambda;
+                    }
                 }
             }
+            int k = (int) (edges.second - edges.first);
+            return 2.0 * (double) lambda / (double) (k * (k - 1));
         }
-        int k = (int)(edges.second - edges.first);
-        return 2.0 * (double)lambda / (double)(k * (k - 1));
+
+        double get_clustering_coefficient() const {
+            double rst = 0.0;
+            for (int i = 0; i < Num_Vertices_; ++i) {
+                rst += get_clustering_coefficient(i);
+            }
+            return rst / (double) Num_Vertices_;
+        }
+
+    private:
+        bool **adjacency_matrix_;
+        int *edges_;
+        int **forward_star_;
+    };
+
+    void serialize_WsNetwork(const WsNetwork &w, std::ostream &osm) {
+        osm << w.Num_Vertices_ << std::endl;
+        osm << w.Num_Edges_ << std::endl;
+        osm << w.Regular_K_ << ' ' << w.Reconnection_Pr_ << std::endl;
+        for (int i = 0; i < w.Num_Vertices_; i++) {
+            auto edge = w.get_edges(i);
+            osm << edge.second - edge.first;
+            while (edge.first != edge.second) {
+                osm << ' ' << *(edge.first);
+                ++edge.first;
+            }
+            osm << std::endl;
+        }
     }
 
-    double get_clustering_coefficient() const {
-        double rst = 0.0;
-        for (int i = 0; i < Num_Vertices_; ++i) {
-            rst += get_clustering_coefficient(i);
+    WsNetwork *deserialize_WsNetwork(std::istream &ism) {
+        int num_vertices, num_edges, regular_k;
+        double reconnection_pr;
+        ism >> num_vertices >> num_edges >> regular_k >> reconnection_pr;
+        int *edge = new int[num_edges];
+        int *degree = new int[num_vertices];
+        int *point = edge;
+        for (int i = 0; i < num_vertices; i++) {
+            ism >> degree[i];
+            for (int j = 0; j < degree[i]; j++) {
+                ism >> *point;
+                ++point;
+            }
         }
-        return rst / (double)Num_Vertices_;
+        return new WsNetwork(num_vertices, num_edges, regular_k, reconnection_pr, edge, degree);
     }
-
-private:
-    bool **adjacency_matrix_;
-    int *edges_;
-    int **start_;
-    int **end_;
-};
-
+}
 #endif //E_PTD_PS_WS_NETWORK_H
--- test/test_ws_network.cpp
+++ test/test_ws_network.cpp
@@ -1,12 +1,12 @@
 /**
  * @file test_ws_network.cpp
- * @version v0.1
+ * @version v0.2
  * @author SHawnHardy
- * @date 2019-01-28
+ * @date 2019-01-31
  * @copyright MIT License
  */
 
-#include <cmath>
+#include <cstring>
 #include <algorithm>
 #include <iostream>
 
@@ -14,10 +14,31 @@
 
 #include "ws_network.h"
 
+void cmpAdjacencyMatrixAndForwardStar(const sh::WsNetwork &w) {
+    int v = w.Num_Vertices_;
+    for (int i = 0; i < v; i++) {
+        auto edge = w.get_edges(i);
+        for (int j = 0; j < v; j++) {
+            if (*(edge.first) == j) {
+                ASSERT_TRUE(w.check(i, j));
+                ++edge.first;
+            } else {
+                ASSERT_FALSE(w.check(i, j));
+            }
+        }
+        ASSERT_EQ(edge.first, edge.second);
+    }
+}
+
 TEST(WsNetworkTest, RegularNetwork) {
     const int Num_Vertices = 200;
     const int Regular_K = 10;
-    WsNetwork tmp(Num_Vertices, Regular_K, -1.0);
+    sh::WsNetwork tmp(Num_Vertices, Regular_K, -1.0);
+    ASSERT_EQ(tmp.Num_Vertices_, Num_Vertices);
+    ASSERT_EQ(tmp.Num_Edges_, Num_Vertices * Regular_K * 2);
+    ASSERT_EQ(tmp.Regular_K_, Regular_K);
+    ASSERT_NEAR(tmp.Reconnection_Pr_, -1.0, 1E-8);
+    cmpAdjacencyMatrixAndForwardStar(tmp);
 
     for (int i = 0; i < Num_Vertices; i++) {
         auto edges = tmp.get_edges(i);
@@ -28,6 +49,7 @@ TEST(WsNetworkTest, RegularNetwork) {
             int a = std::min(i, j);
             int b = std::max(i, j);
             ASSERT_LE(std::min(b - a, a + Num_Vertices - b), Regular_K);
+            ASSERT_TRUE(tmp.check(i, j));
             ++edges.first;
         }
     }
@@ -36,7 +58,7 @@ TEST(WsNetworkTest, RegularNetwork) {
 TEST(WsNetworkTest, ClusteringCoefficient) {
     const int Num_Vertices = 200;
     const int Regular_K = 10;
-    WsNetwork tmp(Num_Vertices, Regular_K, -1.0);
+    sh::WsNetwork tmp(Num_Vertices, Regular_K, -1.0);
     ASSERT_LT(tmp.get_clustering_coefficient() -
               (3.0 * Regular_K - 3.0) / (4.0 * Regular_K - 2.0), 1E-8);
 }
@@ -50,11 +72,41 @@ TEST(WsNetworkTest, WsNetwork) {
                               (1.0 - pr) * (1.0 - pr) * (1.0 - pr);
         double x = 0.0;
         for (int i = 0; i < 20; ++i) {
-            WsNetwork tmp(Num_Vertices, Regular_K, pr);
+            sh::WsNetwork tmp(Num_Vertices, Regular_K, pr);
+            ASSERT_EQ(tmp.Num_Vertices_, Num_Vertices);
+            ASSERT_EQ(tmp.Num_Edges_, Num_Vertices * Regular_K * 2);
+            ASSERT_EQ(tmp.Regular_K_, Regular_K);
+            ASSERT_NEAR(tmp.Reconnection_Pr_, pr, 1E-8);
+            cmpAdjacencyMatrixAndForwardStar(tmp);
+
             x += tmp.get_clustering_coefficient();
         }
 
-        ASSERT_LT(fabs(x / 20.0 - expect), 0.025);
+        ASSERT_NEAR(x / 20.0, expect, 0.025);
         pr += 0.1;
+    }
+}
+
+TEST(WsNetworkTest, serialize) {
+    const int Num_Vertices = 200;
+    const int Regular_K = 10;
+    const double Reconnection_Pr = 0.2;
+    sh::WsNetwork source(Num_Vertices, Regular_K, Reconnection_Pr);
+
+    std::ostringstream osm;
+    sh::serialize_WsNetwork(source, osm);
+
+    std::istringstream ism(osm.str());
+    sh::WsNetwork *result = sh::deserialize_WsNetwork(ism);
+
+    ASSERT_EQ(source.Num_Vertices_, result->Num_Vertices_);
+    ASSERT_EQ(source.Num_Edges_, result->Num_Edges_);
+    ASSERT_EQ(source.Regular_K_, result->Regular_K_);
+    ASSERT_NEAR(source.Reconnection_Pr_, result->Reconnection_Pr_, 1E-8);
+    cmpAdjacencyMatrixAndForwardStar(*result);
+    for (int i = 0; i < Num_Vertices; i++) {
+        for (int j = 0; j < Num_Vertices; j++) {
+            ASSERT_EQ(source.check(i, j), result->check(i, j));
+        }
     }
 }
