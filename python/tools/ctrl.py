acdcf986763dc68cc8d40d1851b81f89ca31f163
SHawnHardy <shawn_work@outlook.com>
2019-03-01 18:18:29 +0800

    [Update]Q_D.py

--- python/tools/ctrl.py
+++ python/tools/ctrl.py
@@ -14,47 +14,41 @@ import pandas as pd
 
 
 class Ctrl:
-    num_times = 10
-    count = []
     task = []
     df_index = []
     result = []
-    num_processed = 0
 
-    def __init__(self, df, param_label, result_label, csv_path):
+    def __init__(self, df, csv_path, num_times=10):
         self.df = df
-        self.param_label = param_label
-        self.result_label = result_label
         self.csv_path = csv_path
         self.df.info()
+        self.num_times = num_times
 
     def get_task(self):
         task_index = 0
         for index, row in self.df.iterrows():
-            if np.any(np.isnan(row[self.result_label])):
-                self.task.append(row[self.param_label])
+            if np.any(np.isnan(row.drop('ISI'))):
+                self.task.append(row)
                 self.df_index.append(index)
                 self.result.append([])
-                self.count.append(0)
                 yield task_index, self.task[-1]
                 task_index += 1
 
-    def proc(self, index):
-        print(self.task[index])
-        result = pd.DataFrame(self.result[index])
+    def proc(self, task_index):
+        print(self.task[task_index].drop(['Q', 'Qsin', 'Qcos', 'ISI']))
+        result = pd.DataFrame(self.result[task_index])
         print(result)
         result = np.mean(result)
         print(result)
         for label, data in result.iteritems():
-            self.df.loc[self.df_index[index], label] = data
+            self.df.loc[self.df_index[task_index], label] = data
 
         self.df = self.df.round(6)
         self.df.to_csv(self.csv_path, index=False, float_format='%.6f')
-        self.num_processed += 1
 
-    def add(self, index, result):
-        self.result[index].append(result)
-        self.count[index] += 1
-        if self.count[index] == self.num_times:
-            self.proc(index)
-        return self.num_processed == len(self.task)
+    def add(self, task_index, result):
+        self.result[task_index].append(result)
+        if len(self.result[task_index]) == self.num_times:
+            self.proc(task_index)
+            return True
+        return False
