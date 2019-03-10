b493b36bf6239e0c61a757db24c66bbb423b9133
SHawnHardy <shawn_work@outlook.com>
2019-03-10 12:11:55 +0800

    [Update]time_delay_matrix, add normal_dtb

--- src/solve.cpp
+++ src/solve.cpp
@@ -27,11 +27,15 @@ struct GlobalArgs {
 //    time_delay_matrix
     enum TimeDelayMatrixType {
         all_the_same,
-        partial
+        partial,
+        normal_distribution
     };
     TimeDelayMatrixType time_delay_matrix_type = all_the_same;
     int time_delay = 0;
     double partial_time_delay_pr = 2.0;
+    double normal_distribution_mean = 3.0;
+    double normal_distribution_standard_deviation = 1.0;
+    int normal_distribution_max = 50 * 1000;
 
 //    fhn_model_config
     double noise_intensity = 0.001;
@@ -42,16 +46,17 @@ struct GlobalArgs {
 };
 
 
-static const char *optString = "vs:D:T:P:";
+static const char *optString = "vs:D:T:P:N";
 
 static const struct option long_opts[] = {
-        {"verbose",               0, nullptr, 'v'},
-        {"size",                  1, nullptr, 's'},
-        {"noise_intensity",       1, nullptr, 'D'},
-        {"tau",                   1, nullptr, 'T'},
-        {"partial_time_delay_pr", 1, nullptr, 'P'},
-        {"log_pulse",             1, nullptr, 301},
-        {"dry_run",               0, nullptr, 401}
+        {"verbose",                        0, nullptr, 'v'},
+        {"size",                           1, nullptr, 's'},
+        {"noise_intensity",                1, nullptr, 'D'},
+        {"tau",                            1, nullptr, 'T'},
+        {"partial_time_delay_pr",          1, nullptr, 'P'},
+        {"normal_distribution_time_delay", 0, nullptr, 'N'},
+        {"log_pulse",                      1, nullptr, 301},
+        {"dry_run",                        0, nullptr, 401}
 };
 
 int main(int argc, char **argv) {
@@ -76,6 +81,9 @@ int main(int argc, char **argv) {
                 global_args.time_delay_matrix_type = global_args.partial;
                 global_args.partial_time_delay_pr = std::atof(optarg);
                 break;
+            case 'N':
+                global_args.time_delay_matrix_type = global_args.normal_distribution;
+                break;
             case 401:
                 global_args.dry_run = true;
                 global_args.verbose = true;
@@ -95,17 +103,26 @@ int main(int argc, char **argv) {
     if (global_args.verbose) {
         std::cout << "size = " << global_args.size << std::endl;
         std::cout << "noise intensity = " << global_args.noise_intensity << std::endl;
-        std::cout << "time delay = " << global_args.time_delay << " * 0.001 s ";
-        switch (global_args.time_delay_matrix_type) {
-            case global_args.all_the_same:
-                std::cout << "(all the same)" << std::endl;
-                break;
-            case global_args.partial:
-                std::cout << "(partial; probability = " << global_args.partial_time_delay_pr << " )" << std::endl;
-                break;
-            default:
-                std::cout << "error" << std::endl;
+
+        if (global_args.time_delay_matrix_type == global_args.normal_distribution) {
+            std::cout << "normal distribution time delay [0, " << global_args.normal_distribution_max << "] * 0.001 s"
+                      << std::endl;
+            std::cout << "mean = " << global_args.normal_distribution_mean << std::endl
+                      << "standard deviation = " << global_args.normal_distribution_standard_deviation << std::endl;
+        } else {
+            std::cout << "time delay = " << global_args.time_delay << " * 0.001 s ";
+            switch (global_args.time_delay_matrix_type) {
+                case global_args.all_the_same:
+                    std::cout << "(all the same)" << std::endl;
+                    break;
+                case global_args.partial:
+                    std::cout << "(partial; probability = " << global_args.partial_time_delay_pr << " )" << std::endl;
+                    break;
+                default:
+                    std::cout << "error" << std::endl;
+            }
         }
+
 
         std::cout << "log pulse: " << (global_args.log_pulse ? "ENABLED" : "DISABLED") << std::endl;
         if (global_args.log_pulse) {
@@ -119,11 +136,17 @@ int main(int argc, char **argv) {
     sh::WsNetwork network(global_args.size, global_args.regular_k, global_args.reconnection_pr);
 
     sh::TimeDelayMatrix tau(global_args.size);
-    if (global_args.time_delay > 0) {
-        if (global_args.time_delay_matrix_type == global_args.all_the_same) {
+    if (global_args.time_delay_matrix_type == global_args.normal_distribution) {
+        tau.normalDistributionTimeDelay(global_args.normal_distribution_mean,
+                                        global_args.normal_distribution_standard_deviation,
+                                        global_args.normal_distribution_max
+        );
+    } else {
+        if (global_args.time_delay > 0) {
             tau.allTheSame(global_args.time_delay);
-        } else if (global_args.time_delay_matrix_type == global_args.partial) {
-            tau.partialTimeDelay(global_args.partial_time_delay_pr, global_args.time_delay);
+            if (global_args.time_delay_matrix_type == global_args.partial) {
+                tau.partial(global_args.partial_time_delay_pr);
+            }
         }
     }
 
